078508b84cc337d18a26f104ef35fd72f958986b
Julian Gruber <julian@juliangruber.com>
2013-01-14 16:42:53 +0100

    initial

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var request = require('request')
+
+var app = express()
+
+app.get(/(http|https):\/\/([a-z0-9\.\/\?]+)/, function (req, res, next) {
+  var url = req.params[0] + '://' + req.params[1]
+  
+  var opts = {}
+  if ('timeout' in req.query) opts.timeout = req.query.timeout
+  
+  request(url, opts, function (err, response, body) {
+    var color = 'red'
+    if (err && err.code.match(/TIMEDOUT/)) color = 'yellow'
+    if (!err || response.statusCode == 200) color = 'green'
+    
+    res.sendfile(__dirname + '/images/' + color + '.png')
+  })
+})
+
+app.listen(process.env.PORT || 4000)

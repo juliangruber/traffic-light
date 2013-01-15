bb0e7b745aeb34c321f91bbae35650e109fa3ff2
Julian Gruber <julian@juliangruber.com>
2013-01-15 14:02:43 +0100

    travis

---
+++ test/server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+app.get('/green', function (req, res) {
+  res.send('ok')
+})
+
+app.get('/red', function (req, res) {
+  res.status(500).end('error')
+})
+
+app.listen(9990)
--- test/test.js
+++ test/test.js
@@ -9,9 +9,12 @@ var red = fs.readFileSync(__dirname + '/../images/red.png')
 var yellow = fs.readFileSync(__dirname + '/../images/yellow.png')
 var green = fs.readFileSync(__dirname + '/../images/green.png')
 
-// color (err, status, dt) -> color
+// make sure the server is running
+require('./server')
 
 describe('color', function () {
+  // color (err, status, dt) -> color
+  
   it('should show red', function () {
     color({}, 200, -1).should.equal('red')
     color(null, 500, -1).should.equal('red')
@@ -29,7 +32,7 @@ describe('color', function () {
 describe('service', function () {
   it('should be red when an error occurs', function (done) {
     request(light)
-      .get('/"http://localhost:99999"/?timeout=0')
+      .get('/"http://asdjleifjsludhgkuhldijvlidjkusrhf.com"/?timeout=0')
       .expect('Content-Type', 'image/png')
       .expect(200)
       .end(function (err, res) {
@@ -40,61 +43,37 @@ describe('service', function () {
   })
 
   it('should be red when a bad status code is returned', function (done) {
-    var app = express()
-    app.get('/', function (req, res) {
-      res.status(500).end('error')
-    })
-
-    var server = app.listen(9990)
-
     request(light)
-      .get('/"http://localhost:9990"/?timeout=0')
+      .get('/"http://localhost:9990/red"/?timeout=0')
       .expect('Content-Type', 'image/png')
       .expect(200)
       .end(function (err, res) {
         if (err) return done(err)
         res.text.should.equal(red.toString())
-        server.close()
         done()
       })
   })
   
   it('should be yellow when the server takes too long to respond', function (done) {
-    var app = express()
-    app.get('/', function (req, res) {
-      res.end('ok')
-    })
-    
-    var server = app.listen(9990)
-    
     request(light)
-      .get('/"http://localhost:9990"/?timeout=0')
+      .get('/"http://localhost:9990/green"/?timeout=0')
       .expect('Content-Type', 'image/png')
       .expect(200)
       .end(function (err, res) {
         if (err) return done(err)
         res.text.should.equal(yellow.toString())
-        server.close()
         done()
       })
   })
   
   it('should be green when the server responds in time', function (done) {
-    var app = express()
-    app.get('/', function (req, res) {
-      res.end('ok')
-    })
-    
-    var server = app.listen(9990)
-    
     request(light)
-      .get('/"http://localhost:9990"/')
+      .get('/"http://localhost:9990/green"/')
       .expect('Content-Type', 'image/png')
       .expect(200)
       .end(function (err, res) {
         if (err) return done(err)
         res.text.should.equal(green.toString())
-        server.close()
         done()
       })
   })
